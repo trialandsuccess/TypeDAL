272e7359b8977d0e6c3600446c09384958249ae4
Robin van der Noord <robinvandernoord@gmail.com>
2022-11-29 16:35:19 +0100

    v0.1.1

---
+++ README.md
@@ -0,0 +1,6 @@
+# TypeDAL
+
+Typing support for [PyDAL](http://web2py.com/books/default/chapter/29/6)
+
+More documentation following soon, compare example_old.py (pydal) with example_new.py (typedal) to get an idea of how it
+works.
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+setup(name='TypeDal',
+      version='0.1.1',
+      description='Typing support for PyDAL',
+      author='Robin van der Noord',
+      author_email='contact@trialandsuccess.nl',
+      url='https://github.com/trialandsuccess/TypeDAL',
+      packages=['typedal'],
+      long_description=long_desc,
+      long_description_content_type="text/markdown",
+      install_requires=["pydal"],
+      python_requires='>3.10.0',
+      )
