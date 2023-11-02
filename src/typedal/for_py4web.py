d355314686aebf57b0fd2babeae6c4cd07207264
Robin van der Noord <robinvandernoord@gmail.com>
2023-11-02 15:57:00 +0100

    feat(p4w): add AuthUser class

--- src/typedal/for_py4web.py
+++ src/typedal/for_py4web.py
@@ -1,14 +1,14 @@
 """
 ONLY USE IN COMBINATION WITH PY4WEB!
 """
-
-from typing import Any
+from datetime import datetime
+from typing import Any, Optional
 
 import json_fix  # noqa: F401
 import threadsafevariable
 from py4web.core import ICECUBE, Fixture
 
-from .core import TypeDAL
+from .core import TypeDAL, TypedTable
 
 
 class DAL(TypeDAL, Fixture):  # pragma: no cover
@@ -34,3 +34,24 @@ class DAL(TypeDAL, Fixture):  # pragma: no cover
         Commit db on success.
         """
         self.recycle_connection_in_pool_or_close("commit")
+
+
+class AuthUser(TypedTable):
+    """
+    Class for db.auth_user in py4web (probably not w2p).
+    """
+    redefine = True
+    migrate = False
+
+    # call db.define on this when ready
+
+    email: str
+    password: str
+    first_name: Optional[str]
+    last_name: Optional[str]
+    sso_id: Optional[str]
+    action_token: Optional[str]
+    last_password_change: Optional[datetime]
+    past_passwords_hash: Optional[str]
+    username: Optional[str]
+    phone_number: Optional[str]
