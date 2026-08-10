f34d4c4e932873527ccb3aa3258ea2d1551a49bc
Robin van der Noord <robinvandernoord@gmail.com>
2026-08-10 12:43:52 +0200

    chore: switch to `vommit`

--- src/typedal/__about__.py
+++ src/typedal/__about__.py
@@ -5,4 +5,6 @@ This file contains the Version info for this package.
 # SPDX-FileCopyrightText: 2023-present Robin van der Noord <robinvandernoord@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "4.9.12"
+from importlib.metadata import version
+
+__version__ = version("TypeDAL")
